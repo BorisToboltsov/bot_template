8f4ed9d5b696a4c4cae0d096f736c7b88063f472
BorisToboltsov <b.toboltsov@vcore.su>
2023-02-27 11:24:50 +0300

    init

---
+++ secret.py
@@ -0,0 +1,8 @@
+SECRET_KEY = ""  # Django secret key
+SQL_ENGINE = "dj_db_conn_pool.backends.postgresql"
+SQL_DATABASE = ""  # Database name
+SQL_USER = ""  # Sql user name
+SQL_PASSWORD = ""  # Sql user password
+SQL_HOST = "localhost"
+TELEGRAM_TOKEN = ""  # Telegram token
+SENTRY_DSN = "http://test@192.168.0.1:9000/3"  # Connect from Sentry
